73453fb2dd2d98f83bdceb5197bbea0db3161253
ranieri72 <ranieri72@hotmail.com>
2017-06-07 23:28:28 -0300

    Correção do link do host

--- app/src/main/java/com/myband/myband/asyncTask/UserTask.java
+++ app/src/main/java/com/myband/myband/asyncTask/UserTask.java
@@ -23,7 +23,7 @@ public class UserTask extends AsyncTask<Bundle, Void, User> {
     @Override
     protected User doInBackground(Bundle... params) {
         OkHttpClient client = new OkHttpClient();
-        String host = "http://myband.ddns.net/myband/rest/user";
+        String host = "http://myband.ddns.net/rest/user";
         //final String host = "http://192.168.15.6:8080/myband/rest/user";
         MediaType json = MediaType.parse("application/json; charset=utf-8");
         Gson gson = new Gson();
